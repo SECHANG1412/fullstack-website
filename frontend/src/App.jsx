d74c5319521e378767daa29121a854492c19972c
SECHANG1412 <binaryblitz1412@gmail.com>
2025-09-09 14:51:27 +0900

    App.jsx routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,59 @@
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import MainPage from './Page/MainPage/MainPage';
+import About from './Page/About/About';
+import Board from './Page/Board/Board';
+import Contact from './Page/Contact/Contact';
+import Leadership from './Page/Leadership/Leadership';
+import Services from './Page/Services/Services';
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/board',
+        element: <Board />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/leadership',
+        element: <Leadership />,
+      },
+      {
+        path: '/our-services',
+        element: <Services />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Footer />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
